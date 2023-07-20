d8627c6feb4028efa7f16bbc569a5d7980274b18
PabloOrtiz07 <portiz@frba.utn.edu.ar>
2023-07-20 18:18:13 -0300

    subo cambios

--- cypress/e2e/1-getting-started/login.cy.js
+++ cypress/e2e/1-getting-started/login.cy.js
@@ -3,12 +3,13 @@ import loginPage from "../../pages/login.js"
 
 describe('Logueo de pagina',()=>{
   beforeEach(()=>{
-    cy.visit('https://automationteststore.com/index.php?rt=account/login')
+    cy.visit('https://automationteststore.com/')
   })
 
 
   it('logueo valido',()=>{
     const login = new loginPage();
+    login.getLoginIndex().click();
     login.getUsuarioInput().type("preuba07");
     login.getPasswordInput().type("papapa123")
     login.getLogin().click();
--- cypress/pages/login.js
+++ cypress/pages/login.js
@@ -1,21 +1,30 @@
 
 class loginLocators{
     constructor(){
+        //ul[@id='main_menu_top']
         //this.loginBtn = '[id="loginFrm"]';
+        ////ul[@id='customer_menu_top']//a[@href='https://automationteststore.com/index.php?rt=account/login']
+        this.loginIndex='[id="customer_menu_top"]';
         this.loginBtn='[title="Login"]';
         this.userInput = '[id="loginFrm_loginname"]';
         this.passwordInput = '[id="loginFrm_password"]';
     }
 }
 
+
 export default class login {
     constructor(){
         this.locators=new loginLocators();
     }
 
+    getLoginIndex(){
+        return cy.get(this.locators.loginIndex);
+    }
+
     getLogin(){
         return cy.get(this.locators.loginBtn);
     }
+
     getUsuarioInput(){
         return cy.get(this.locators.userInput);
     }
